7a410f14b94f7a8eca3c4fcb36d36a67d3e0ae88
Rushikesh Jawale <rushikeshjawale2004@gmail.com>
2025-11-22 01:47:35 +0530

    feat(analytics): adds vercel analytics.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import HomePage from "@/pages/Home"
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <div>
       <HomePage />
+      <Analytics />
     </div>
   )
 }
