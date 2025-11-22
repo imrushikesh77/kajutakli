87ae719086a82faeb151a3449b175e3c5aef69fa
Rushikesh Jawale <rushikeshjawale2004@gmail.com>
2025-11-22 13:27:56 +0530

    feat(posthog): Adds posthog analytixcs>

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,17 @@ import './index.css'
 import "./App.css";
 import App from '@/App'
 
+import { PostHogProvider } from 'posthog-js/react'
+
+const options = {
+  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+  defaults: '2025-05-24',
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <PostHogProvider apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY} options={options}>
+      <App />
+    </PostHogProvider>
   </StrictMode>,
 )
